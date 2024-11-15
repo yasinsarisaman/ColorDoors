97826f992e97de1ff6cf01519d75781d2ac8eb3b
yasinsarisaman <yasinsarisaman@firmadefteri.com>
2024-11-15 23:07:21 +0300

    Feat (Levels) : New levels added, dynamic runtime maze material added

--- ColorDoors/Assets/ColorDoors/Scripts/Managers/MazeManager.cs
+++ ColorDoors/Assets/ColorDoors/Scripts/Managers/MazeManager.cs
@@ -6,12 +6,35 @@ using UnityEngine;
 public class MazeManager : MonoBehaviour
 {
     [SerializeField] private Material _mazeSharedMaterial;
+    [SerializeField] private Material _mazeDynamicSharedMaterial;
+    private Color _dynamicMazeColorStart;
+    private Color _dynamicMazeColorEnd;
+    private float _lerpDuration = 1f; 
+    private float _timeElapsed = 0f;
     
     void Start()
     {
         _mazeSharedMaterial.SetColor("_Color", new Color(_mazeSharedMaterial.color.r,_mazeSharedMaterial.color.g,_mazeSharedMaterial.color.b,.5f));
+        _dynamicMazeColorStart = new Color(_mazeDynamicSharedMaterial.color.r, _mazeDynamicSharedMaterial.color.g,
+            _mazeDynamicSharedMaterial.color.b, .1f);
+        _dynamicMazeColorEnd = new Color(_mazeDynamicSharedMaterial.color.r, _mazeDynamicSharedMaterial.color.g,
+            _mazeDynamicSharedMaterial.color.b, .9f);
     }
-    
+
+    private void Update()
+    {
+        if (_timeElapsed < _lerpDuration)
+        {
+            float t = _timeElapsed / _lerpDuration;
+            _mazeDynamicSharedMaterial.SetColor("_Color",Color.Lerp(_dynamicMazeColorStart, _dynamicMazeColorEnd, t));
+            _timeElapsed += Time.deltaTime;
+        }
+        else
+        {
+            _timeElapsed = 0.0f;
+        }
+    }
+
     private void OnEnable()
     {
         EventBus<MazeCollisionEvent>.AddListener(OnMazeCollision);
