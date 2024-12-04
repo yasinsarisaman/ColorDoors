1f968ce5cb29b65b71e6415f468d46842b12f967
yasinsarisaman <yasinsarisaman@firmadefteri.com>
2024-12-04 23:03:19 +0300

    Fix (Player) : locked player rotation x and z

--- ColorDoors/Assets/ColorDoors/Scripts/Doors/OrangeDoor.cs
+++ ColorDoors/Assets/ColorDoors/Scripts/Doors/OrangeDoor.cs
@@ -61,7 +61,7 @@ public class OrangeDoor : MonoBehaviour, Interactable
     private void OpenDoor()
     {
         isOpen = true;
-        transform.position = new Vector3(_initialDoorPosition.x, _initialDoorPosition.y + 1.25f, _initialDoorPosition.z);
+        transform.position = new Vector3(_initialDoorPosition.x, _initialDoorPosition.y - 1.25f, _initialDoorPosition.z);
         
         EventBus<OpenDynamicMazeWalls>.Emit(this,new OpenDynamicMazeWalls(timeToOpenMazeWalls));
     }
